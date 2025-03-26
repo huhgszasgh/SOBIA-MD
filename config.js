720f9508f952ee6799595291e557e0157a78ff95
huhgszasgh <sbcreationofficial595@gmail.com>
2025-03-26 23:07:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "UPl1RQJJ#jjOSDxypaPuTcAA7CHwjqhciRIFAOWMT7jXECbW1NX4",
     CAPTION: process.env.CAPTION || "POWERED BY SOBIA BUTT",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
